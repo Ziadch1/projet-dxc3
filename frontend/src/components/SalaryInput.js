79057302aa41c165461d937a87bc4a28edff360d
Yassir <yassirdiri@gmail.com>
2024-08-16 12:07:52 +0100

    changed more code

--- frontend/src/components/SalaryInput.js
+++ frontend/src/components/SalaryInput.js
@@ -6,7 +6,7 @@ function SalaryInput({ onSalaryChange }) {
   useEffect(() => {
     const fetchSalary = async () => {
       try {
-        const response = await fetch(`http://backend:5000/salary`);
+        const response = await fetch(`http://192.168.59.108:30001/salary`);
         const amount = await response.json();
         setSalary(amount);
         onSalaryChange(amount);
@@ -24,7 +24,7 @@ function SalaryInput({ onSalaryChange }) {
     onSalaryChange(newSalary);
 
     try {
-      await fetch(`http://backend:5000/salary`, {
+      await fetch(`http://192.168.59.108:30001/salary`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
